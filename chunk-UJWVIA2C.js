4e020a2333bd020218d16fceb737874037e2c6d3
iamowaisazam <iamowaisazam@gmail.com>
2025-02-19 14:40:34 +0500

    up

---
+++ chunk-UJWVIA2C.js
@@ -0,0 +1,1 @@
+import{o as n}from"./chunk-J2EGX742.js";import{La as s,Z as e}from"./chunk-PQVSAWMG.js";var m=(()=>{let t=class t{constructor(i){this.sanitizer=i}transform(i){return this.sanitizer.bypassSecurityTrustHtml(i)}};t.\u0275fac=function(a){return new(a||t)(s(n,16))},t.\u0275pipe=e({name:"safeHtml",type:t,pure:!0,standalone:!0});let r=t;return r})();export{m as a};
